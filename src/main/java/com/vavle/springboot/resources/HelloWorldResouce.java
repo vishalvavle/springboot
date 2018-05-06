eef10b56cb717d5d0486c4a3ba1d3441cce7e37e
Vishal Vavle <vishalvavle@Vishals-MacBook-Pro.local>
2018-05-06 11:59:39 -0700

    This is springboot initial commit

---
+++ src/main/java/com/vavle/springboot/resources/HelloWorldResouce.java
@@ -0,0 +1,19 @@
+package com.vavle.springboot.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldResouce {
+
+	@GetMapping("/hello/{name}")
+	public String sayHello(@PathVariable String name) {
+		return "Hello " + name;
+	}
+	
+	@GetMapping("/sum/{a}")
+	public int sum(@PathVariable int a) {
+		return a+ 10;
+	}
+}
